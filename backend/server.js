94882c15fd31f7a17dc2a0cbf490d0319dc014b8
rumailijaz <rumailboss7@gmail.com>
2023-04-19 20:16:26 +0500

    routes and error handling

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import dotenv from  'dotenv'
 import colors from 'colors'
+import logger from './middlewear/logger.js'
+import errorHandler from './middlewear/error.js'
 import connectDB from './config/db.js'
-import products from './data/products.js'
+import productRoutes from './routes/productRouter.js'
 
 dotenv.config()
 
@@ -10,21 +12,18 @@ connectDB()
 
 const app = express()
 
+// Middlewear
+app.use(logger)
+
 app.get('/',(req,res)=>{
     res.send('api is running')
 })
 
-app.get('/api/products',(req,res)=>{
+app.use('/api/products', productRoutes)
 
-    console.log('get products');
-    res.json(products)
-})
-
-app.get('/api/products/:id',(req,res)=>{
-    const product = products.find((p) => p._id===req.params.id)
-    res.json(product)
-})
+// error handling
+app.use(errorHandler)
 
 const PORT =  process.env.PORT || 5000
 
-app.listen(PORT, console.log(`server is running in ${process.env.NODE_ENV} mode on ${PORT} port`.yellow.bold));
+app.listen(PORT, console.log(`server is running in ${process.env.NODE_ENV} mode on ${PORT} port`.yellow.bold )); 
