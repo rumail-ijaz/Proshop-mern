a2615540ebb6b6c39293d7ccdc8d8a900afab101
rumailijaz <rumailboss7@gmail.com>
2023-04-25 22:46:11 +0500

    redux setup and message and loader components and product list and detail reducers

--- backend/routes/productRouter.js
+++ backend/routes/productRouter.js
@@ -1,8 +1,8 @@
 import express from "express";
 import asyncHandler from 'express-async-handler'
-const router = express.Router()
 import Product from "../models/productModel.js";
 
+const router = express.Router()
 
 // @desc Fetch all products
 // @route GET /api/products
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,29 +1,31 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import { Link, useParams } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col, Image,  ListGroup, Card, Button } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import axios from 'axios'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
+import { listProductDetails } from '../actions/productActions'
 
 const ProductScreen = () => {
-    const { id } = useParams();
+  const { id } = useParams();
 
-    const [product, setProduct] = useState({})
+  const dispatch = useDispatch()
 
+  const productDetails = useSelector(state => state.productDetails)
+console.log(productDetails);
+  const { loading, error, product } = productDetails
+  
   useEffect(() => {
-    const fetchProduct = async ()=>{
-      const { data } = await axios.get(`/api/products/${id}`)
-      setProduct(data)
-    }
-
-    fetchProduct()
+   dispatch(listProductDetails(id))
     
-  }, [id])
-
+  }, [dispatch, id])
 
   return (
     <>
         <Link className='btn btn-light my-3' to='/'> Go Back</Link>
-        <Row>
+        {loading ? <Loader/> : error ? <Message variant='danger'>{error}</Message> : (
+            <Row>
             <Col md={6}>
                 <Image src={product.image} alt={product.name} fluid/>
             </Col>
@@ -70,6 +72,8 @@ const ProductScreen = () => {
                 </Card>
             </Col>
         </Row>
+        )}
+        
     </>
   )
 }
